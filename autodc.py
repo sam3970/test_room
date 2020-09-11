8b747decbddecf6b48ca518737062a2bb0352cde
SangKyu Lee <sam3970@gmail.com>
2020-09-11 11:52:09 +0900

    testing

---
+++ autodc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+import os
+
+class add_User():
+        user_id = null
+        user_passwd = null
+        
+        def create():
+                while
+
